99a8ac6e61b8be391f4001ad8361755e55c9fcd2
Quyyy <quyanhn100@gmail.com>
2026-01-21 01:35:52 +0100

    yes

--- script.js
+++ script.js
@@ -129,7 +129,7 @@ const mobileQuery = window.matchMedia("(max-width: 768px)");
 
 function handleMobileChange(e) {
   if (e.matches) {
-    document.querySelector(".content").innerHTML = "Use desktop instead.";
+    document.querySelector(".content").innerHTML = "<p style='text-align: center'>Mobile function not yet implemented.<br>Use desktop instead.</p>";
   } else {
 
   }
